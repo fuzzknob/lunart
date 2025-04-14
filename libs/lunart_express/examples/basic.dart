da21aaff1a60ce6fdeee85acf739b92f694a6c3d
Gagan Rai <gagan_rai@outlook.com>
2025-04-14 22:29:34 +0545

    feat: improve basic example

--- libs/lunart_express/examples/basic.dart
+++ libs/lunart_express/examples/basic.dart
@@ -4,6 +4,24 @@ void main() async {
   // Initialize a SQLite database connection to 'sqlite.db' file
   final db = Database.init(SqliteConnection.file('sqlite.db'));
 
+  // Creating table
+  await db.executeRaw('''
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY,
+      name TEXT NOT NULL,
+    );
+  ''');
+
+  // INSERT
+  // inserts a single row
+  await db.table('users').insert({'name': 'Greg Guy'});
+
+  // inserts multiple rows
+  await db.table('users').insertMany([
+    {'name': 'Greg Gal'},
+    {'name': 'Victor Eee'},
+  ]);
+
   // SELECT
   final allUsers = await db.table('users').all();
   print(allUsers);
@@ -15,16 +33,6 @@ void main() async {
   // Runs "SELECT * FROM users WHERE id = 2" and returns all matching rows
   final user2 = await db.table('users').where('id', 2).get();
   print(user2);
-
-  // INSERT
-  // inserts a single row
-  await db.table('users').insert({'name': 'Greg Guy'});
-
-  // inserts multiple rows
-  await db.table('users').insertMany([
-    {'name': 'Greg Gal'},
-    {'name': 'Victor Eee'},
-  ]);
 
   // UPDATE
   await db.table('users').where('id', 1).update({'name': 'John Doe'});

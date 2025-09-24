facc363b9ef94f70f668e53919718ea6fb87ad17
Gagan Rai <gagan_rai@outlook.com>
2025-09-24 21:21:29 +0545

    refactor: move plugin typedef to types.dart

--- libs/lunart_core/lib/src/types.dart
+++ libs/lunart_core/lib/src/types.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 
 import 'server/server.dart';
 
+typedef Plugin = Function(Server server);
+
 typedef Next = FutureOr<Response> Function();
 typedef Handler = FutureOr<Response> Function(Request request);
 typedef Middleware = FutureOr<Response> Function(Request request, Next next);
